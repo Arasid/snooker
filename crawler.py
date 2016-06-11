755717adb79a9cfff701eb40fee024a76d4fd57c
Barbora Klembarova <basa.akrobat@gmail.com>
2016-06-11 23:06:01 +0200

    dokoncene crawlovanie

--- crawler.py
+++ crawler.py
@@ -8,7 +8,7 @@ import sys
 import create_db
 
 # parameters
-years = [2016]
+years = [2016, 2015, 2014, 2013, 2012, 2011, 2010, 2009, 2008, 2007]
 category = 'Ranking'
 category_col_name = 'Category'
 status = 'Professional'
@@ -18,33 +18,114 @@ tour_col_name = 'Tournament'
 conn = sqlite3.connect('db.sqlite3')
 c = conn.cursor()
 
+def get_date(text):
+    return time.mktime(datetime.datetime.strptime(text, '%Y-%m-%d').timetuple())
+
+def parse_eng_date(text):
+    if text == 'Unavailable':
+        return -1
+    date = re.sub(r'(\d)(st|nd|rd|th)', r'\1', text.strip())
+    return time.mktime(datetime.datetime.strptime(date, '%B %d %Y').timetuple())
+
 def parse_dates(text):
     res = []
     for x in text.split('-'):
-        date = re.sub(r'(\d)(st|nd|rd|th)', r'\1', x.strip())
-        date = time.mktime(datetime.datetime.strptime(date, "%B %d %Y").timetuple())
-        res.append(date)
+        res.append(parse_eng_date(x))
+    if len(res) == 1:
+        res.append(res[-1])
     return res
 
-def crawl_round(round_title, round_tds):
-    print "crawling Round: %s" % round_title
+def get_player(td):
+    name = td.getText(strip=True)
+    c.execute('SELECT id FROM players WHERE name=?', (name,))
+    player = c.fetchone()
+    if player is None:
+        print 'Crawling player: %s' % name
+        # create player
+        url = td['href']
+        f = urllib2.urlopen(url)
+        time.sleep(1)
+        html = f.read()
+        soup = BeautifulSoup(html, 'html.parser')
+        th = soup.find('th', text='Player Information')
+        tbody = th.find_parent().find_parent().select_one('tbody')
+        country = tbody.find('td', text='Country').find_next_sibling().getText(strip=True)
+        professional_year = tbody.find('td', text='Turned professional').find_next_sibling().getText(strip=True)
+        birth = tbody.find('td', text='Date of birth').find_next_sibling().getText(strip=True)
+        birth = parse_eng_date(' '.join(birth.split()[:3]))
+        c.execute('INSERT INTO players(id, name, birthdate, country, professional) VALUES (NULL, ?, ?, ?, ?)',
+                        (name, birth, country, professional_year))
+        return c.lastrowid
+    else:
+        return player[0]
+
+def crawl_round(round_id, round_tds):
+    print 'crawling Round: %s' % round_id
+    for round_td in round_tds:
+        row = round_td.find_parent()
+        player1_td = row.select_one('td.match_player1 a')
+        player2_td = row.select_one('td.match_player2 a')
+        player1 = get_player(player1_td)
+        player2 = get_player(player2_td)
+        player1_score = row.select_one('td.match_player1_score').getText(strip=True)
+        player2_score = row.select_one('td.match_player2_score').getText(strip=True)
+        bestof = row.select_one('td.match_best_of').getText(strip=True)
+        # su tam () kolo cisla (asi vzdy, im not sure)
+        bestof = re.match(r'[\(]?(\d*)[\)]?', bestof).group(1)
+
+        x = re.compile('.*(Walkover).*')
+        walkover = 0
+        for i in [1,2]:
+            walk = row.select_one('td.match_player%s' % i).getText(strip=True)
+            res = x.match(walk)
+            if res is not None:
+                walkover = i
+
+        date = 'NULL'
+        frames = ''
+        if walkover == 0:
+            next_row = row.find_next_sibling()
+            if next_row.select_one('td.match_round') is None:
+                td = next_row.select_one('td')
+                date_text = td.getText('|||', strip=True)
+                if date_text != "":
+                    date_text = date_text.split('|||')[1]
+                    # ak je tam range, tak zatial na nho kaslem ... lebo ani nie je pri vsetkych zapasoch
+                    # pri ktorych by mal byt
+                    date_text = date_text.split(' - ')[0]
+                    date = get_date(date_text)
+
+                frames = td.find_next_sibling().getText('|||', strip=True)
+                if frames != '':
+                    frames = frames.split('|||')[1]
+
+        c.execute('''
+                    INSERT INTO matches(id, score1, score2, date, round, player1, player2, bestof, walkover, frames)
+                    VALUES (NULL, ?, ?, ?, ?, ?, ?, ?, ?, ?)''', 
+                    (player1_score, player2_score, date, round_id, player1, player2, bestof, walkover, frames))
 
 def crawl_tour(tour_url, title):
-    print "crawling Tournament: %s" % title
+    print 'crawling Tournament: %s' % title
     f = urllib2.urlopen(tour_url)
     time.sleep(1)
     html = f.read()
     soup = BeautifulSoup(html, 'html.parser')
-    x = soup.find("td", text="Dates: ")
+    x = soup.find('td', text='Dates: ')
     dates = x.find_next_sibling().text
     start, end = parse_dates(dates)
-    x = soup.find("td", text="Location: ")
+    x = soup.find('td', text='Qualifying dates: ')
+    dates = x.find_next_sibling().text
+    qstart, qend = 'NULL', 'NULL'
+    if dates != 'No qualifier dates available':
+        qstart, qend = parse_dates(dates)
+    x = soup.find('td', text='Location: ')
     location = x.find_next_sibling().text
-    c.execute("INSERT INTO tournaments(id, name, location, startdate, enddate) VALUES (NULL, ?, ?, ?, ?)", (title, location, start, end))
+    c.execute('''INSERT INTO tournaments(id, name, location, startdate, enddate, qualstartdate, qualenddate) 
+                VALUES (NULL, ?, ?, ?, ?, ?, ?)''', (title, location, start, end, qstart, qend))
     tour_id = c.lastrowid
     conn.commit()
 
-    tds = soup.select("td.match_round")
+    tds = soup.select('td.match_round')
     rounds = []
     rounds_tds = []
     for td in tds:
@@ -54,18 +135,19 @@ def crawl_tour(tour_url, title):
             rounds_tds.append([])
         rounds_tds[-1].append(td)
     for i in range(len(rounds)):
-        print rounds[i], i, tour_id
-        c.execute("INSERT INTO rounds(id, name, roundorder, tournament) VALUES (NULL, ?, ?, ?)", (rounds[i], i, tour_id))
-        crawl_round(rounds[i], rounds_tds[i])
+        c.execute('INSERT INTO rounds(id, name, roundorder, tournament) VALUES (NULL, ?, ?, ?)', (rounds[i], i, tour_id))
+        round_id = c.lastrowid
+        crawl_round(round_id, rounds_tds[i])
+        conn.commit()
 
 def crawl_year(year):
     year_url = 'http://cuetracker.net/Tournaments/%s' % year
     f = urllib2.urlopen(year_url)
     time.sleep(1)
-    print "crawling YEAR", year_url
+    print 'crawling YEAR', year_url
     html = f.read()
     soup = BeautifulSoup(html, 'html.parser')
-    f = soup.select_one("#main_table")
+    f = soup.select_one('#main_table')
     ## aby som vedela ze ktory stlpec co znamena
     col_names = [x.text for x in f.select('thead > th')]
     tour_col = col_names.index(tour_col_name)
@@ -85,15 +167,13 @@ def crawl_year(year):
         if tds[status_col].text != status:
             continue
         url = tds[tour_col].select_one('a').get('href')
-        tours.append((url, tds[tour_col].text.strip()))
+        tours.append((url, tds[tour_col].getText(strip=True)))
 
     for tour, title in tours:
         crawl_tour(tour, title)
 
-#for year in years:
-#    crawl_year(year)
-
-crawl_tour("http://cuetracker.net/Tournaments/World-Championship/2016/2063", "2016 World Championship")
+for year in years:
+    crawl_year(year)
 
 conn.commit()
 conn.close()
--- create_db.py
+++ create_db.py
@@ -4,13 +4,18 @@ conn = sqlite3.connect('db.sqlite3')
 c = conn.cursor()
 c.execute('''CREATE TABLE IF NOT EXISTS players (
                 id integer primary key,
-                name text )''')
+                name text,
+                birthdate integer,
+                country text,
+                professional text)''')
 c.execute('''CREATE TABLE IF NOT EXISTS tournaments (
                 id integer primary key,
                 name text,
                 location text,
                 startdate integer,
-                enddate integer )''')
+                enddate integer,
+                qualstartdate integer,
+                qualenddate integer )''')
 c.execute('''CREATE TABLE IF NOT EXISTS rounds (
                 id integer primary key,
                 name text,
@@ -19,14 +24,15 @@ c.execute('''CREATE TABLE IF NOT EXISTS rounds (
                 FOREIGN KEY(tournament) REFERENCES tournaments(id) )''')
 c.execute('''CREATE TABLE IF NOT EXISTS matches (
                 id integer primary key,
-                result1 integer,
-                result2 integer,
+                score1 integer,
+                score2 integer,
                 date integer, 
                 round integer,
                 player1 integer,
                 player2 integer,
                 bestof integer,
                 walkover integer,
+                frames text,
                 FOREIGN KEY(round) REFERENCES rounds(id),
                 FOREIGN KEY(player1) REFERENCES players(id),
                 FOREIGN KEY(player2) REFERENCES players(id) )''')
