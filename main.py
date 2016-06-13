b4d93f31175314560f11cf6f6b4d5bb8ae5540cf
Barbora Klembarova <basa.akrobat@gmail.com>
2016-06-13 18:22:49 +0200

    pills pridane do playera + calendar

--- main.py
+++ main.py
@@ -22,18 +22,24 @@ def teardown_request(exception):
 def stamp2text(time):
     return datetime.datetime.fromtimestamp(int(time)).strftime('%d.%m.%Y')
 
-#def get_days(times):
-#    days = {}
-#    for time in times:
-#        dat = datetime.datetime.fromtimestamp(int(time))
-#        day = (dat.year, dat.month, dat.day)
-#        if not day in days:
-#            days[day] = 0
-#        days[day] += 1
-#    res = []
-#    for k,v in days.items():
-#        res.append([k[0],k[1],k[2],v])
-#    return res
+def get_days(times):
+    days = {}
+    years = set()
+    for time in times:
+        if time == 'NULL':
+            continue
+        dat = datetime.datetime.fromtimestamp(int(time))
+        if dat.year < datetime.datetime.now().year - 4:
+            continue
+        day = (dat.year, dat.month, dat.day)
+        years.add(dat.year)
+        if not day in days:
+            days[day] = 0
+        days[day] += 1
+    res = []
+    for k,v in days.items():
+        res.append([k[0],k[1],k[2],v])
+    return res, len(years)
 
 @app.route('/tournament/<id>/')
 def tournament(id):
@@ -100,7 +106,7 @@ def player(id):
         'professional': player[3]
     }
 
-    cur.execute('SELECT m.score1,m.score2,m.player1,m.player2,p1.name,p2.name,m.bestof,m.walkover,m.round, r.name, t.id, t.name FROM matches as m, players as p1, players as p2, rounds as r, tournaments as t WHERE m.player1 = p1.id AND m.player2 = p2.id AND m.round = r.id AND r.tournament = t.id AND (m.player1 = ? OR m.player2 = ?) ORDER BY t.name DESC;', (id, id))
+    cur.execute('SELECT m.score1,m.score2,m.player1,m.player2,p1.name,p2.name,m.bestof,m.walkover,m.round, r.name, t.id, t.name,m.date FROM matches as m, players as p1, players as p2, rounds as r, tournaments as t WHERE m.player1 = p1.id AND m.player2 = p2.id AND m.round = r.id AND r.tournament = t.id AND (m.player1 = ? OR m.player2 = ?) ORDER BY t.name DESC;', (id, id))
     matches = cur.fetchall()
     cur.execute("SELECT id,name,roundorder FROM rounds WHERE tournament = ? ORDER BY roundorder ASC;", (id,))
     rounds = cur.fetchall()
@@ -108,7 +114,8 @@ def player(id):
     results = []
     last_tid = -1
     last_rid = -1
-    for score1, score2, id1, id2, player1, player2, bestof, walkover, round, round_name, t_id, t_name in matches:
+    times = []
+    for score1, score2, id1, id2, player1, player2, bestof, walkover, round, round_name, t_id, t_name, date in matches:
         if t_id != last_tid:
             last_tid = t_id
             results.append({
@@ -133,8 +140,11 @@ def player(id):
             'bestof': bestof,
             'walkover': walkover
         })
+        times.append(date)
 
-    return render_template('player.html', info=info, results=results)
+    days,n_years = get_days(times)
+
+    return render_template('player.html', info=info, results=results, days=days, n_years=n_years)
 
 @app.route('/player/')
 def players():
